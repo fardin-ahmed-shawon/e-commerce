4d87fdf16b5cf5cc90ef8af4263987490c4142ad
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-02-13 23:03:23 +0600

    Search Functionality Added

--- js/search.js
+++ js/search.js
@@ -279,7 +279,6 @@ const products = [
         "images": "img/products/womens/ladiespant/ladiespant3.jpg"
     }
 ]
-
 // Generate keywords array from product titles
 let keywords = products.map(product => product.title);
 
@@ -290,8 +289,8 @@ inputBox.onkeyup = function() {
     let result = [];
     let input = inputBox.value;
     if (input.length) {
-        result = keywords.filter((keyword) => {
-            return keyword.toLowerCase().includes(input.toLowerCase());
+        result = products.filter((product) => {
+            return product.title.toLowerCase().includes(input.toLowerCase());
         });
     }
     display(result);
@@ -299,8 +298,8 @@ inputBox.onkeyup = function() {
 
 function display(result) {
     if (result.length) {
-        const content = result.map((list) => {
-            return `<li><a class="dropdown-item" href="#">${list}</a></li>`;
+        const content = result.map((item) => {
+            return `<li><a class="dropdown-item" href="#" onclick="openProduct('${item.id}', '${item.title}')">${item.title}</a></li>`;
         }).join('');
         resultBox.innerHTML = content;
         resultBox.style.display = 'block';
@@ -308,4 +307,11 @@ function display(result) {
         resultBox.innerHTML = '';
         resultBox.style.display = 'none';
     }
+}
+
+function openProduct(id, title) {
+    // Store the product ID and title in localStorage or pass them as query parameters
+    localStorage.setItem('selectedProductId', id);
+    localStorage.setItem('selectedProductTitle', title);
+    window.location.href = 'product.html';
 }
